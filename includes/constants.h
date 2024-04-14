7fd8ae5226171d514f07d230c0fe75d4f04f02cf
monsieurjune <june.monsieurjune@gmail.com>
2024-04-14 17:49:24 +0700

    update cub3d's header files

--- includes/constants.h
+++ includes/constants.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   constants.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dklimkin <dklimkin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 17:06:01 by dklimkin          #+#    #+#             */
-/*   Updated: 2024/04/09 17:06:44 by dklimkin         ###   ########.fr       */
+/*   Updated: 2024/04/14 13:19:01 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,13 @@
 
 # define SUCCESS 0
 # define FAILURE -1
+# define EXIT_SUCCESS 0
+# define EXIT_FAILURE 1
+# define NORTH 'N'
+# define SOUTH 'S'
+# define WEST 'W'
+# define EAST 'E'
+# define EMPTY '0'
+# define WALL '1'
 
 #endif
---
+++ includes/parser.h
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/12 18:01:21 by tponutha          #+#    #+#             */
+/*   Updated: 2024/04/14 13:31:22 by tponutha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSER_H
+# define PARSER_H
+# include <fcntl.h>
+# include <stdbool.h>
+# include "constants.h"
+# include "../libs/libft/includes/libft.h"
+
+typedef struct s_texture
+{
+	void	*img;
+	int		width;
+	int		height;
+}	t_texture;
+
+typedef struct s_rgb
+{
+	unsigned char	r;
+	unsigned char	g;
+	unsigned char	b;
+	bool			isinit;
+}	t_rgb;
+
+typedef enum e_direction
+{
+	undefine,
+	north,
+	south,
+	west,
+	east
+}	t_direction;
+
+typedef struct s_player
+{
+	int			x;
+	int			y;
+	t_direction	face;
+}	t_player;
+
+typedef struct s_parser
+{
+	void		*mlx;
+	void		*window;
+	t_texture	north;
+	t_texture	south;
+	t_texture	east;
+	t_texture	west;
+	t_rgb		floor;
+	t_rgb		ceil;
+	char		**map;
+	int			width;
+	int			height;
+	t_player	player;
+}	t_parser;
+
+int	open_cub3d(char *path);
+// int	parser_init(void *mlx, int fd, t_);
+int	get_element(t_parser *info, int fd);
+
+#endif
